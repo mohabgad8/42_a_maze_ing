3861a5f3e7000d0fe857bff5129594e345d06bfd
mohabgad8 <mohabgadpro@gmail.com>
2026-03-03 09:02:35 +0100

    Add config parsing and validation

--- config.py
+++ config.py
@@ -5,111 +5,132 @@
         * Parsing the data received
         * Validation of the different data
 """
+import config_helper as helper
+import sys
 
 
-def parse_config(filename: str) -> dict[str, str]:
+def get_config(filename: str) -> dict[str, str]:
     """
         Parse config.txt and returns a dict containing different data.
     """
 
     config: dict[str, str] = {}
+    required_keys: set[str] = {
+        'width', 'height', 'entry', 'exit', 'output_file', 'perfect'}
     try:
         with open(filename, 'r') as file:
             for line in file:
                 line: str = line.strip()
+
                 if not line or line.startswith('#'):
                     continue
+
                 if '=' not in line:
                     raise ValueError(
                         f"Error: '{line}' is invalid: doesn't contain '='")
 
-                parts: list[str] = line.split('=', 1)
-                key: str = parts[0].strip()
-                value: str = parts[1].strip()
-
+                key, value = line.split('=', 1)
                 if not key or not value:
                     raise ValueError(
                         f"Error: key or value missing for '{line}'")
-                config[key] = value
-        print(config)
+                config[key.strip().lower()] = value.strip()
+
+        missing = required_keys - config.keys()
+        if missing:
+            raise ValueError(f"Error: missing parameter {missing}")
         return config
     except FileNotFoundError:
         raise FileNotFoundError(f"Error: file '{filename}' not found")
     except PermissionError:
-        raise PermissionError(f"Error: don't have access to '{filename}'")
+        raise PermissionError(f"Error: can't have access to '{filename}'")
     except OSError:
         raise OSError(f"Error: something went wrong with '{filename}'")
 
 
-def valid_config(
-        parsed_config: dict[str, str]) -> dict[
+def get_valid_config(
+        config: dict[str, str]) -> dict[
             str, str | bool | int | tuple[int, int]]:
     """
-        Returns a bool based on the validation of parsed config.
+        Manipulates the parsed config dict and verifies if width/height/entry
+        and exit are correctly formatted. Once the verifications are made,
+        it will return a dict containing the different data.
 
         Validation key points:
         ----------------------
         * Width and height aren't negatives. They are also within
             the limits of the maze. Which means: not too small to be able
             to contain the "42" logo.
+        * Entry and exit are within the actual maze and not beyond limits.
+            They must be different.
     """
 
-    MIN_WIDTH: int = 5
-    MIN_HEIGHT: int = 5
-
-    if not parsed_config:
+    if not config:
         raise ValueError("Error: Something went wrong with parsed config.")
 
-    validated_config: dict[
+    valid_config: dict[
         str, str | bool | int | tuple[int, int]] = {}
     # Verify if width and height are correct
     try:
-        width: int = int(parsed_config['WIDTH'])
-        height: int = int(parsed_config['HEIGHT'])
+        width: int = int(config['width'])
+        height: int = int(config['height'])
 
-        # In case 42 logo is 5x5
-        if width < MIN_WIDTH or height < MIN_HEIGHT:
-            raise ValueError("Error: Maze is too small fort the '42' logo.")
-
-        validated_config['WIDTH'] = width
-        validated_config['HEIGHT'] = height
-
-        def parse_coords()
     except ValueError:
         raise ValueError("Error: Measurements are incorrect !")
     except KeyError:
         raise KeyError("Error: Key doesn't exist")
 
+    # In case 42 logo is 5x5
+    if not helper.verify_size(width, height):
+        raise ValueError("Error: Maze is too small fort the '42' logo.")
+
+    valid_config['width'] = width
+    valid_config['height'] = height
+
     # Get entry's coord.
     try:
-        entry: str = parsed_config['ENTRY']
-        entry_x: int = int((entry.split(',', 1)[0]))
-        entry_y: int = int((entry.split(',', 1)[1]))
-    except ValueError:
-        raise ValueError("Error: entry's coordinates must be integers")
+        entry_x, entry_y = helper.divide_coords(config['entry'])
+    except ValueError as e:
+        raise ValueError(f"{e}")
 
-    # Check if entry's coord. are within limits
-    if entry_x <= 0 or entry_y <= 0 or entry_x >= width or entry_y >= height:
-        raise ValueError("Error: Coordinates are incorrect !")
+    if not helper.verify_coords(entry_x, entry_y, width, height):
+        raise ValueError("Error: entry's coordinates are invalid !")
+    valid_config['entry'] = (entry_x, entry_y)
 
     # Get exit's coord.
     try:
-        end: str = parsed_config['EXIT']
-        end_x: int = int((end.split(',', 1)[0]))
-        end_y: int = int((end.split(',', 1)[1]))
-    except ValueError:
-        raise ValueError("Error: exit's coordinates must be integers")
+        exit_x, exit_y = helper.divide_coords(config['exit'])
+    except ValueError as e:
+        raise ValueError(f"{e}")
 
-    # Check if exit's coord. are within limits
-    if end_x <= 0 or end_y <= 0 or end_x >= width or end_y >= height:
-        raise ValueError("Error: Coordinates are incorrect !")
+    if not helper.verify_coords(exit_x, exit_y, width, height):
+        raise ValueError("Error: exit's coordinates are invalid !")
+    valid_config['exit'] = (exit_x, exit_y)
 
     # Check if entry and exit are similar
-    if entry == end:
+    if entry_x == exit_x and entry_y == exit_y:
         raise ValueError("Error: Entry and Exit can't be identical !")
 
-    return validated_config
+    # Output_file
+    try:
+        valid_config['output_file'] = config['output_file']
+    except KeyError:
+        raise KeyError("Error: output_file key is missing in config")
+
+    # Perfect
+    if config['perfect'].lower() == 'true':
+        valid_config['perfect'] = True
+    elif config['perfect'].lower() == 'false':
+        valid_config['perfect'] = False
+    else:
+        raise ValueError("Error: perfect's parameter is invalid !")
+
+    return valid_config
 
 
 if __name__ == "__main__":
-    parse_config("config.txt")
+    try:
+        config = get_config("config.txt")
+        valid_config = get_valid_config(config)
+    except Exception as e:
+        print(f"{e}")
+        sys.exit(1)
